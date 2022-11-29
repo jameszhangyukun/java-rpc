fa8a2f68c715d9e3a5ddbee1f2eb83e845cd02b6
zhangyukun <38148677+jameszhangyukun@users.noreply.github.com>
2022-11-29 11:00:07 +0800

    add tranport entity definition

--- rpc-framework/src/main/java/com/zyk/config/CustomShutdownHook.java
+++ rpc-framework/src/main/java/com/zyk/config/CustomShutdownHook.java
@@ -1,11 +1,17 @@
 package com.zyk.config;
 
+import com.zyk.registry.zk.util.CuratorUtils;
+import com.zyk.remoting.transport.netty.server.NettyRpcServer;
+import com.zyk.util.concurrent.threadpool.ThreadPoolFactoryUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
+/**
+ * When the server  is closed, do something such as unregister all services
+ */
 @Slf4j
 public class CustomShutdownHook {
     private static final CustomShutdownHook CUSTOM_SHUTDOWN_HOOK = new CustomShutdownHook();
@@ -17,9 +23,15 @@ public class CustomShutdownHook {
     public void clearAll() {
         log.info("addShutdownHook for clearAll");
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            // TODO Close Netty
+            try {
+                InetSocketAddress inetSocketAddress = new InetSocketAddress(
+                        InetAddress.getLocalHost().getHostAddress(), NettyRpcServer.PORT
+                );
+                CuratorUtils.clearRegistry(CuratorUtils.getZkClient(), inetSocketAddress);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            ThreadPoolFactoryUtil.shutdownAllThreadPool();
         }));
     }
-
-
 }
---
+++ rpc-framework/src/main/java/com/zyk/remoting/transport/netty/server/NettyRpcServer.java
@@ -0,0 +1,57 @@
+package com.zyk.remoting.transport.netty.server;
+
+import com.zyk.config.CustomShutdownHook;
+import com.zyk.util.RuntimeUtil;
+import com.zyk.util.concurrent.threadpool.ThreadPoolFactoryUtil;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import lombok.SneakyThrows;
+
+import java.net.InetAddress;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Server. Receive the client message, call the corresponding method according to the client message,
+ * and then return the result to the client.
+ */
+public class NettyRpcServer {
+    public static final int PORT = 9998;
+
+    @SneakyThrows
+    public void start() {
+        CustomShutdownHook.getCustomShutdownConfig().clearAll();
+        String host = InetAddress.getLocalHost().getHostAddress();
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        DefaultEventExecutorGroup serviceHandlerGroup = new DefaultEventExecutorGroup(
+                RuntimeUtil.cpus() * 2,
+                ThreadPoolFactoryUtil.createThreadFactory("service-handler-group", false)
+        );
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new IdleStateHandler(30, 0, 0, TimeUnit.SECONDS));
+                    }
+                });
+
+    }
+}
