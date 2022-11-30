6946b58013c5fb4da1c5352684f952882e8d428c
zhangyukun <38148677+jameszhangyukun@users.noreply.github.com>
2022-11-30 11:20:28 +0800

    add rpc server

--- rpc-framework/src/main/java/com/zyk/remoting/transport/netty/codec/RpcMessageDecoder.java
+++ rpc-framework/src/main/java/com/zyk/remoting/transport/netty/codec/RpcMessageDecoder.java
@@ -17,8 +17,17 @@ import java.util.Arrays;
 
 @Slf4j
 public class RpcMessageDecoder extends LengthFieldBasedFrameDecoder {
-    public RpcMessageDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) {
-        super(maxFrameLength, lengthFieldOffset, lengthFieldLength);
+    public RpcMessageDecoder() {
+        // lengthFieldOffset: magic code is 4B, and version is 1B, and then full length. so value is 5
+        // lengthFieldLength: full length is 4B. so value is 4
+        // lengthAdjustment: full length include all data and read 9 bytes before, so the left length is (fullLength-9). so values is -9
+        // initialBytesToStrip: we will check magic code and version manually, so do not strip any bytes. so values is 0
+        this(RpcConstants.MAX_FRAME_LENGTH, 5, 4, -9, 0);
+    }
+
+    public RpcMessageDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength,
+                             int lengthAdjustment, int initialBytesToStrip) {
+        super(maxFrameLength, lengthFieldOffset, lengthFieldLength, lengthAdjustment, initialBytesToStrip);
     }
 
     private Object decodeFrame(ByteBuf in) {
--- rpc-framework/src/main/java/com/zyk/remoting/transport/netty/server/NettyRpcServer.java
+++ rpc-framework/src/main/java/com/zyk/remoting/transport/netty/server/NettyRpcServer.java
@@ -1,14 +1,13 @@
 package com.zyk.remoting.transport.netty.server;
 
 import com.zyk.config.CustomShutdownHook;
+import com.zyk.remoting.transport.netty.codec.RpcMessageDecoder;
+import com.zyk.remoting.transport.netty.codec.RpcMessageEncoder;
 import com.zyk.util.RuntimeUtil;
 import com.zyk.util.concurrent.threadpool.ThreadPoolFactoryUtil;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -17,6 +16,7 @@ import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetAddress;
 import java.util.concurrent.TimeUnit;
@@ -25,6 +25,7 @@ import java.util.concurrent.TimeUnit;
  * Server. Receive the client message, call the corresponding method according to the client message,
  * and then return the result to the client.
  */
+@Slf4j
 public class NettyRpcServer {
     public static final int PORT = 9998;
 
@@ -38,20 +39,34 @@ public class NettyRpcServer {
                 RuntimeUtil.cpus() * 2,
                 ThreadPoolFactoryUtil.createThreadFactory("service-handler-group", false)
         );
-        ServerBootstrap serverBootstrap = new ServerBootstrap();
-        serverBootstrap.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childOption(ChannelOption.TCP_NODELAY, true)
-                .childOption(ChannelOption.SO_KEEPALIVE, true)
-                .option(ChannelOption.SO_BACKLOG, 128)
-                .handler(new LoggingHandler(LogLevel.INFO))
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        ChannelPipeline pipeline = socketChannel.pipeline();
-                        pipeline.addLast(new IdleStateHandler(30, 0, 0, TimeUnit.SECONDS));
-                    }
-                });
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.TCP_NODELAY, true)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast(new IdleStateHandler(30, 0, 0, TimeUnit.SECONDS));
+                            pipeline.addLast(new RpcMessageEncoder());
+                            pipeline.addLast(new RpcMessageDecoder());
+                            pipeline.addLast(serviceHandlerGroup, new NettyRpcServerHandler());
+                        }
+                    });
 
+            ChannelFuture f = serverBootstrap.bind(host, PORT).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("occur exception when start server:", e);
+        } finally {
+            log.error("shutdown bossGroup and workerGroup");
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+            serviceHandlerGroup.shutdownGracefully();
+        }
     }
 }
